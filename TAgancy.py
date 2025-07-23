0e9b3802384131d91cbdfe4a8ccb60a54658813f
CDVC <cesarcamposeco415@gmail.com>
2025-07-23 10:48:24 -0600

    Mostrar y ingresar al 100

--- TAgancy.py
+++ TAgancy.py
@@ -1,4 +1,5 @@
 def Menu():
+    print(" ")
     print("Bienvenido a la agencia de viajes Bing Bong")
     print("1.Agregar clientes")
     print("2.Listado de clientes y destinos")
@@ -20,30 +21,36 @@ try:
                     print("La cantidad ingresada no es valida")
                 else:
                     for i in range(number):
-                        C_code = f"CL+{cont}"
+                        C_code = f"CL{cont}"
                         clients[C_code] = {}
                         name = input("Ingrese el nombre del cliente: ")
                         clients[C_code]["name"] = name
+                        clients[C_code]["Destiny"] = {}
                         traveln = int(input("Ingrese cuántos destinos desea ingresar: "))
                         if traveln <= 0 or traveln >5:
                             print("La cantidad ingresada no es valida")
                         else:
                             for i in range(traveln):
-                                D_code = f"D+{cont1}"
+                                D_code = f"DY{cont1}"
                                 destiny = input(f"Ingrese el destino no.{i+1}: ")
-                                clients[D_code]["Destiny"][D_code] = {destiny}
+                                clients[C_code]["Destiny"][D_code] = {"destination": destiny}
                                 cont1 = cont1 + 1
                         cont = cont + 1
+                    print(" ")
             case 2:
                 show_c = 1
                 for code, value in clients.items():
                     des_c = 1
+                    print(" ")
                     print(f"Cliente {show_c}")
                     print(f"Código de cliente: {code}")
                     print(f"Nombre del cliente: {value['name']}")
                     print(f"Destinos del cliente {show_c}")
                     for code, value in value["Destiny"].items():
-                        print(f"Destino {des_c}: {value['destiny']}")
+                        print(f"Destino {des_c}: {value['destination']}")
+                        des_c = des_c + 1
+                    show_c = show_c + 1
+                print(" ")
 
             case 3:
                 print("Ver")
